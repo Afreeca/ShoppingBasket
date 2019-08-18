7eb6ef943a6c1821298b64097364bb77f0549814
Afreeca  Mendes <adilson.mendes@capco.com>
2019-08-18 19:00:24 +0100

    refact

---
+++ src/test/java/com/adilsonmendes/test/api/adthena/repository/ProductRepositoryTest.java
@@ -0,0 +1,42 @@
+package com.adilsonmendes.test.api.adthena.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductRepositoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addProduct() {
+    }
+
+    @Test
+    public void getProduct() {
+    }
+
+    @Test
+    public void isProductExists() {
+    }
+
+    @Test
+    public void isProductExists1() {
+    }
+
+    @Test
+    public void getProductList() {
+    }
+
+    @Test
+    public void print() {
+    }
+}
